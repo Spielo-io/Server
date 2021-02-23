799f1bfef813de961367d318e2b507cb2c2ac64b
Andreas <lukesaltweather@gmail.com>
2021-02-23 16:13:49 +0100

    Add Lobby and Lobbycontroller

---
+++ src/io/spielo/Lobby.java
@@ -0,0 +1,54 @@
+package io.spielo;
+
+import io.spielo.client.ServerClient;
+import io.spielo.messages.CreateLobbyMessage;
+import io.spielo.messages.Message;
+
+import java.util.Random;
+
+public class Lobby {
+    private ServerClient host;
+    private ServerClient player2;
+    private String code;
+    private Boolean isPublic;
+    private byte game;
+    private byte timer;
+    private byte bestOf;
+
+    public Lobby(ServerClient host, CreateLobbyMessage message) {
+        this.host = host;
+        this.isPublic = message.getPublic();
+        this.game = message.getGame();
+        this.timer = message.getTimer();
+        this.bestOf = message.getBestOf();
+    }
+
+    public void onPlayerJoin(ServerClient player){
+        this.player2 = player;
+    }
+
+    public String generateRandomCode() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 6;
+        Random random = new Random();
+
+        String rand = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return this.code = rand;
+    }
+    public void sendOtherPlayer(short sender, Message message) {
+        if(sender == host.getID()){
+            player2.send(message);
+        }
+        else {
+            host.send(message);
+        }
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+}
---
+++ src/io/spielo/LobbyController.java
@@ -0,0 +1,63 @@
+package io.spielo;
+
+import io.spielo.client.ServerClient;
+import io.spielo.messages.*;
+import io.spielo.messages.types.MessageType1;
+import io.spielo.messages.types.MessageType2Lobby;
+
+import java.util.HashMap;
+
+public class LobbyController implements Subscriber{
+
+    private final HashMap<String, Lobby> codeLobbyMap;
+    private final HashMap<Short, Lobby> idLobbyMap;
+
+    LobbyController() {
+        this.codeLobbyMap = new HashMap<>();
+        this.idLobbyMap = new HashMap<>();
+    }
+
+    @Override
+    public void onMessageReceived(ServerClient sender, Message message) {
+        if (message instanceof CreateLobbyMessage) {
+            this.handleCreateLobbyMsg(sender, (CreateLobbyMessage) message);
+        }
+        else if(message instanceof JoinLobbyMessage) {
+            this.handleJoinLobbyMsg(sender, (JoinLobbyMessage) message);
+        }
+        else {
+            this.handleGameMessage(sender, message);
+        }
+    }
+
+    @Override
+    public void onClientLostConnection(ServerClient sender) {
+
+    }
+
+    public void handleCreateLobbyMsg(ServerClient sender, CreateLobbyMessage message) {
+        Lobby lobby = new Lobby(sender, message);
+        String code = lobby.generateRandomCode();
+        codeLobbyMap.put(code, lobby);
+        idLobbyMap.put(sender.getID(), lobby);
+        MessageHeader header = new MessageHeader((short)0, sender.getID(), MessageType1.LOBBY, MessageType2Lobby.JOIN, System.currentTimeMillis());
+        sender.send(new CreateLobbyResponseMessage(header, code));
+    }
+
+    public void handleJoinLobbyMsg(ServerClient sender, JoinLobbyMessage message) {
+        Lobby lobby = codeLobbyMap.get(message.getCode());
+        if(lobby == null){
+            sender.send(new JoinLobbyResponseMessage(sender.getID(), 0)); //failed
+            return;
+        }
+        lobby.onPlayerJoin(sender);
+        idLobbyMap.put(sender.getID(), lobby);
+        sender.send(new JoinLobbyResponseMessage(sender.getID(), 1)); //success
+    }
+
+    public void handleGameMessage(ServerClient sender, Message message) {
+        Lobby lobby = idLobbyMap.get(message.getHeader().getReceiverID());
+        lobby.sendOtherPlayer(sender.getID(), message);
+    }
+
+}
